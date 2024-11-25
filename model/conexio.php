320d7ab694bf82148e2755ceb12b947e877d66f7
Marcos Lopez Medina <m.lopez5@sapalomera.cat>
2024-11-25 17:23:28 +0100

    Fix require path for environment configuration in conexio.php

--- model/conexio.php
+++ model/conexio.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '../env.php';
+require_once __DIR__ . '/../env.php';
 
 function connexio() {
     //Dades connexio a BD.
@@ -11,7 +11,6 @@ function connexio() {
     //Connexió.
     try {
         $connexio = new PDO("mysql:host=$host;dbname=$nomBD", $usuari, $contra);
-        $connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connexio;
     } catch (PDOException $e) {
         die("Error: " . $e->getMessage());
