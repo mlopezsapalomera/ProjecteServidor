f4f1e3e0d4ca0f442d8bcb4aab67687d4df9c3e9
Marcos Lopez Medina <m.lopez5@sapalomera.cat>
2024-11-25 17:05:12 +0100

    Update database password in environment configuration

--- env.php
+++ env.php
@@ -2,5 +2,5 @@
 define('DB_HOST', 'bbdd.marcoslopez.cat');
 define('DB_NAME', 'ddb239154');
 define('DB_USER', 'ddb239154');
-define('DB_PASS', '4{W{3j}CJRF%RF');
+define('DB_PASS', 'pJ[pk(gLcy6$cm');
 ?>
