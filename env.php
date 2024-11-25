546e68e090a1f3815697d1be4a54c386ad8cd7b4
Marcos Lopez Medina <m.lopez5@sapalomera.cat>
2024-11-25 16:57:04 +0100

    Update database user credentials in environment configuration

--- env.php
+++ env.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'db239154.dns-marcoslopez.cat');
 define('DB_NAME', 'ddb239154');
-define('DB_USER', 'ddb239154');
+define('DB_USER', 'ftp.marcoslopez.cat');
 define('DB_PASS', '4{W{3j}CJRF%RF');
 ?>
