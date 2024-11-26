3bbaef5ed56df2d57689429130cc2f5c8215eb0e
Marcos Lopez Medina <m.lopez5@sapalomera.cat>
2024-11-26 16:27:19 +0100

    Remove .gitignore and update env.php to simplify database connection settings

--- env.php
+++ env.php
@@ -1,16 +1,6 @@
 <?php
-// Para conexion localhost
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'ddb239154');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-
-/* Para conexion con servidor
     define('DB_HOST', 'bbdd.marcoslopez.cat');
     define('DB_NAME', 'ddb239154');
     define('DB_USER', 'ddb239154');
     define('DB_PASS', 'pJ[pk(gLcy6$cm');
-*/
 ?>
-
-
