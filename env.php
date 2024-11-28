3d404d5b9af2abc472c17a76e37e80b8c989c0e2
Marcos Lopez Medina <m.lopez5@sapalomera.cat>
2024-11-28 17:36:03 +0100

    Update database connection settings: change DB_HOST, DB_USER, and DB_PASS for production environment

--- env.php
+++ env.php
@@ -1,6 +1,7 @@
 <?php
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', 'bbdd.marcoslopez.cat');
     define('DB_NAME', 'ddb239154');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_USER', 'ddb239154');
+    define('DB_PASS', 'pJ[pk(gLcy6$cm');
+
 ?>
