72374f5d9dfd56481ee3c569793187515f9a7b79
m.lopez5 <m.lopez5@sapalomera.cat>
2025-01-28 18:46:12 +0100

    Refactor SQL dump for database `ddb239154`: update table structures, add foreign key constraints, and insert initial data for `pokemons` and `usuarios`

--- model/ddb239154.sql
+++ model/ddb239154.sql
@@ -1,70 +1,203 @@
--- Crear la base de datos
-CREATE DATABASE IF NOT EXISTS `ddb239154` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `ddb239154`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-01-2025 a las 18:43:10
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ddb239154`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `friends`
+--
+
+CREATE TABLE `friends` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `password_resets`
+--
+
 CREATE TABLE `password_resets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `token` varchar(64) NOT NULL,
-  `expiry` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `expiry` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pokemons`
+--
+
 CREATE TABLE `pokemons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `imatge` varchar(255) NOT NULL,
   `descripció` text DEFAULT NULL,
-  `usuario_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `usuario_id` (`usuario_id`)
+  `força` int(11) DEFAULT NULL,
+  `dany` int(11) DEFAULT NULL,
+  `vida` int(11) DEFAULT NULL,
+  `tipus` varchar(100) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Volcado de datos para la tabla `pokemons`
+--
+
+INSERT INTO `pokemons` (`id`, `nom`, `imatge`, `descripció`, `força`, `dany`, `vida`, `tipus`, `usuario_id`) VALUES
+(3, 'squirtle', 'lobo-solitario.png', NULL, 48, 50, 44, 'water', 24),
+(2, 'ivysaur', 'images.jpeg', NULL, 62, 80, 60, 'grass, poison', 25),
+(4, 'bulbasaur', 'st,small,507x507-pad,600x600,f8f8f8.jpg', NULL, 49, 65, 45, 'grass, poison', 24),
+(5, 'weedle', 'Fm6zfFAWIAE6vCG.jpg', NULL, 35, 20, 40, 'bug, poison', 24),
+(6, 'venomoth', 'lobo-solitario.png', NULL, 65, 90, 70, 'bug, poison', 24),
+(7, 'metapod', 'lobo-solitario.png', NULL, 20, 25, 50, 'bug', 24),
+(8, 'bulbasaur', 'st,small,507x507-pad,600x600,f8f8f8.jpg', NULL, 49, 65, 45, 'grass, poison', 24),
+(9, 'venusaur', 'lobo-solitario.png', NULL, 82, 100, 80, 'grass, poison', 24);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user_tokens`
+--
+
 CREATE TABLE `user_tokens` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `token` varchar(32) NOT NULL,
-  `expiry` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `expiry` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
+--
+
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `rol` varchar(20) NOT NULL,
-  `imagen` varchar(255) DEFAULT 'default.jpg',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  `imagen` varchar(255) DEFAULT 'default.jpg'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Estructura de tabla para la tabla `friends`
-CREATE TABLE `friends` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `friend_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE,
-  FOREIGN KEY (`friend_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+--
+-- Volcado de datos para la tabla `usuarios`
+--
 
+INSERT INTO `usuarios` (`id`, `nom`, `email`, `password`, `rol`, `imagen`) VALUES
+(24, 'Marrkitus', 'mxrcol18@gmail.com', '$2y$10$4vL50zQZrKJCXQIyKP9f7.IYTII2abjXGbPjkQmiFw4lxKlQJLlw.', '', 'default.jpg'),
+(25, 'User Pruebas', 'user1@gmail.com', '$2y$10$Nf/lZF4Wlhz.FDpGdPfjxeZZELcrcnjVgHGXZVPUIvaSd/Uy2AVjG', '', 'default.jpg');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `friend_id` (`friend_id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pokemons`
+--
+ALTER TABLE `pokemons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `user_tokens`
+--
+ALTER TABLE `user_tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `password_resets`
+--
 ALTER TABLE `password_resets`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- AUTO_INCREMENT de la tabla `pokemons`
+--
 ALTER TABLE `pokemons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
+--
+-- AUTO_INCREMENT de la tabla `user_tokens`
+--
 ALTER TABLE `user_tokens`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `friends`
+--
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
